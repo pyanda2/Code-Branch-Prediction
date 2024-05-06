2e3b26f86bcf4fd2c1aaf23847088cc80f12c6ff
rohitc33 <70339497+rohitc33@users.noreply.github.com>
2024-05-05 19:54:23 -0500

    Add t test to evaluation script

--- runtime-analysis/evaluation/evaluation.py
+++ runtime-analysis/evaluation/evaluation.py
@@ -3,6 +3,9 @@ import subprocess
 import sys
 import time
 
+import numpy as np
+from scipy.stats import ttest_ind
+
 PATH = ""
 dirs = os.path.realpath(__file__).split("/")
 for dir in dirs[:-2]:
@@ -29,8 +32,8 @@ def run_project(path, running_times, runs=1):
     path_elems = path.split("/")
     project_name = path_elems[-1]
     type = path_elems[-2]
-    images = ["ocean", "mountain"]
-    # images = ["ocean", "forest", "mountain"]
+    # images = ["ocean", "mountain"]
+    images = ["ocean", "forest", "mountain"]
     # Run run times
 
     os.system("make clean")
@@ -45,15 +48,10 @@ def run_project(path, running_times, runs=1):
             result = subprocess.run(["/bin/bash", "-c", f"time {command}"], env=env, stderr=subprocess.PIPE).stderr
 
             if not f"{project_name}_{image}" in running_times:
-                running_times[f"{project_name}_{image}"] = [[], 0]
+                running_times[f"{project_name}_{image}"] = []
 
-            # utime = result.split()[0].decode("utf-8")
-            # cycles = int(result.decode("utf-8").split("\n")[16].split()[0])
             utime = float(result.decode("utf-8").split()[0])
-            running_times[f"{project_name}_{image}"][type == "groundtruths"].append(utime)
-
-        # Compute Average
-        # running_times[f"{project_name}_{image}"][type == "groundtruths"] /= runs
+            running_times[f"{project_name}_{image}"].append(utime)
 
     return running_times
 
@@ -80,13 +78,18 @@ if __name__ == "__main__":
         unlabeled_times = run_project(path, unlabeled_times, runs)
 
     for project in running_times.keys():
+        unlabeled = unlabeled_times[project]
+        generated = running_times[project]
+
+        t, p = ttest_ind(unlabeled, generated)
         print(
             f"""{project}: 
-              [ unlabeled: {unlabeled_times[project][0]}
-                generated: {running_times[project][0]} ]"""
+              [ unlabeled: (mean={np.mean(unlabeled)}, std={np.std(unlabeled)}) {unlabeled}
+                generated: (mean={np.mean(generated)}, std={np.std(generated)}) {generated}
+                p-value: {p} ]"""
         )
 
         with open(f"{project}.csv", "a") as f:
             f.write("unlabeled, generated")
-            for v1, v2 in zip(unlabeled_times[project][0], running_times[project][0]):
+            for v1, v2 in zip(unlabeled_times[project], running_times[project]):
                 f.write(f"{v1}, {v2}")
